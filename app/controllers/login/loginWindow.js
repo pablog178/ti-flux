d179d0cb4131c1176f057549f5cb656f7a5276bc
Pablo Guevara <pablog.178@gmail.com>
2017-10-03 16:55:01 -0500

    Add login window

---
+++ app/controllers/login/loginWindow.js
@@ -0,0 +1,32 @@
+/**
+ * Controller-View for the Login Window
+ * @class Controllers.login.loginWindow
+ */
+
+var args = arguments[0] || {};
+
+const LOG_TAG = '\x1b[31m' + '[controllers/login/loginWindow]' + '\x1b[39;49m ';
+
+/**
+ * @method init
+ * @private
+ * Initializes the controller
+ * @return {void}
+ */
+function init () {
+	doLog && console.log(LOG_TAG, '- init');
+}
+
+// +-------------------
+// | Public members.
+// +-------------------
+
+// +-------------------
+// | Private members.
+// +-------------------
+
+// +-------------------
+// | Event Handlers declaration.
+// +-------------------
+
+init();
